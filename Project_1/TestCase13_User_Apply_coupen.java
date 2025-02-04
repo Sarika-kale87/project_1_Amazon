376935e42c3e693aac3edc04fc41ca7cbb615377
Sarika-kale87 <kale.sarika2013@gmail.com>
2025-02-04 09:26:11 +0100

    Project_1_amazon1

--- Project_1/TestCase13_User_Apply_coupen.java
+++
@@ -1,5 +0,0 @@
-package amazon1.Project_1;
-
-public class TestCase13_User_Apply_coupen {
-
-}
--- Project_1/Testcase14_Goto_Orderpage_give_fivestarrating_onlastorder.java
+++
@@ -1,5 +0,0 @@
-package amazon1.Project_1;
-
-public class Testcase14_Goto_Orderpage_give_fivestarrating_onlastorder {
-
-}
